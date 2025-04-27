a92b6b76873ea538f856e7d52191090bc33ad9ed
AliKakoo <alikakuo@yahoo.com>
2025-04-27 21:44:45 +0330

    add new way

--- js/main.js
+++ js/main.js
@@ -1,12 +1,66 @@
-// به دو روش یکی با gsapو یکی بدون gsap
+// روش اول
+// let isFixed = false;
+// let animation;
+// const selector = "nav";
+// const doFix = ()=>{
+
+//     if (!isFixed) {
+//         isFixed = true;
+
+//     gsap.set(selector, {
+//       position: "fixed",
+//       right: 0,
+//       left: 0,
+//       top: 0,
+//       y: "-100%",
+//       zIndex: 20,
+//       opacity: 0,
+//     });
+
+//     animation = gsap
+//       .to(selector, {
+//         y: 0,
+//         opacity: 1,
+//         ease: "power4",
+//         duration: 0.2,
+//         onReverseComplete: () => {
+//           isFixed = false;
+//           gsap.set(selector, { clearProps: "all" });
+//         },
+//       })
+//       .timeScale(0.3);
+
+//     }
+
+// }
+
+// const backToNormal = ()=>{
+
+//     if (isFixed) {
+//         animation.reverse();
+//     }
+// }
+
+// window.addEventListener('scroll', ()=>{
+//     if (window.scrollY>800) {
+//         doFix()
+
+//     }else{
+//         backToNormal()
+//     }
+// },{passive:true})
+
+// روش دوم با scroll trigger
+
 let isFixed = false;
 let animation;
 const selector = "nav";
-const doFix = ()=>{
 
-    if (!isFixed) {
-        isFixed = true;
-        
+ScrollTrigger.create({
+  trigger: selector,
+//   markers: true,
+  start: "top+=800 top",
+  onEnter: () => {
     gsap.set(selector, {
       position: "fixed",
       right: 0,
@@ -29,24 +83,9 @@ const doFix = ()=>{
         },
       })
       .timeScale(0.3);
-
-        
-    }
-
-}
-
-const backToNormal = ()=>{
- 
-    if (isFixed) {
-        animation.reverse();
-    }
-}
-
-window.addEventListener('scroll', ()=>{
-    if (window.scrollY>800) {
-        doFix()
-        
-    }else{
-        backToNormal()
-    }
-},{passive:true})
+  },
+  onLeaveBack: () => {
+    animation?.timeScale(1);
+    animation.reverse();
+  },
+});
