4e7f3aa3a29c36819d3a1b61a558c21da0aac1b3
AliKakoo <alikakuo@yahoo.com>
2025-04-27 20:07:10 +0330

    add animation

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// به دو روش یکی با gsapو یکی بدون gsap
+let isFixed = false;
+let animation;
+const selector = "nav";
+const doFix = ()=>{
+
+    if (!isFixed) {
+        isFixed = true;
+        
+    gsap.set(selector, {
+      position: "fixed",
+      right: 0,
+      left: 0,
+      top: 0,
+      y: "-100%",
+      zIndex: 20,
+      opacity: 0,
+    });
+
+    animation = gsap
+      .to(selector, {
+        y: 0,
+        opacity: 1,
+        ease: "power4",
+        duration: 0.2,
+        onReverseComplete: () => {
+          isFixed = false;
+          gsap.set(selector, { clearProps: "all" });
+        },
+      })
+      .timeScale(0.3);
+
+        
+    }
+
+}
+
+const backToNormal = ()=>{
+ 
+    if (isFixed) {
+        animation.reverse();
+    }
+}
+
+window.addEventListener('scroll', ()=>{
+    if (window.scrollY>800) {
+        doFix()
+        
+    }else{
+        backToNormal()
+    }
+},{passive:true})
